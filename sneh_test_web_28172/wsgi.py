cee0fd285a13374f0e205efb19461939b4acb288
Crowdbotics <team@crowdbotics.com>
2021-06-22 16:25:20 +0000

    Update models

--- sneh_test_web_28172/wsgi.py
+++ sneh_test_web_28172/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sneh_test_web_28172.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sneh_test_web_28172.settings")
 
 application = get_wsgi_application()
